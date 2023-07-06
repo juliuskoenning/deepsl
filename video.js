b45b7020f52790b80f5701dc832e5cbb76a0b322
juliuskoenning <89260388+juliuskoenning@users.noreply.github.com>
2023-07-06 23:04:31 +0200

    add spin circle

--- video.js
+++ video.js
@@ -1,37 +1,48 @@
 function handleVideoUpload(event) {
-    let videoFile = event.target.files[0];
-    let formData = new FormData();
+  let videoFile = event.target.files[0];
+  let formData = new FormData();
 
-    let subtitleText = document.getElementById('subtitle-text');
-    subtitleText.style.opacity = "0";
+  let subtitleText = document.getElementById('subtitle-text');
+  subtitleText.style.opacity = '0';
 
-    formData.append('video', videoFile);
-    fetch('http://localhost:8000/api/videos', {
-        method: 'POST',
-        body: formData
+  formData.append('video', videoFile);
+
+  document.getElementById('loader').style.display = 'block';
+  document.getElementById('upload-label').style.display = 'none';
+
+  fetch('http://localhost:8000/api/videos', {
+    method: 'POST',
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      let videoUrl = URL.createObjectURL(videoFile);
+      let video = document.querySelector('video');
+      video.src = videoUrl;
+      video.load();
+      video.play();
+      video.style.display = 'block';
+
+      data.subtitle.forEach((item) => {
+        setTimeout(function () {
+          subtitleText.textContent = item.label;
+          subtitleText.style.opacity = '1';
+        }, item.start_time * 1000);
+
+        setTimeout(function () {
+          subtitleText.style.opacity = '0';
+        }, item.end_time * 1000);
+      });
+
+      video.oncanplay = function () {
+        document.getElementById('loader').style.display = 'none';
+      };
     })
-    .then(response => response.json())
-    .then(data => {
-        let videoUrl = URL.createObjectURL(videoFile);
-        let video = document.querySelector('video');
-        video.src = videoUrl;
-        video.load();
-        video.play();
-        video.style.display = "block";
-        document.getElementById('upload-label').style.display = 'none';
+    .catch((error) => {
+      console.error(error);
 
-        setTimeout(function() {
-            subtitleText.textContent = data.subtitle;
-            subtitleText.style.opacity = "1";
-        }, 0);
-
-        setTimeout(function() {
-            subtitleText.style.opacity = "0";
-        }, 1000); 
-
-        video.oncanplay = function() {
-            document.getElementById('upload-label').style.display = 'none';
-        };
-    })
-    .catch(error => console.error(error));
+      document.getElementById('loader').style.display = 'none';
+      document.getElementById('upload-label').style.display = 'block';
+    });
 }
